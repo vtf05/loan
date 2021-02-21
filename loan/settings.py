a673868b52e2f19dbf7d4f502dd7609e283c77e6
vtf05 <05avinashvish@gmail.com>
2021-02-20 19:24:22 -0800

    gofor

--- loan/settings.py
+++ loan/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware"
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT= os.path.join(BASE-DIR,"staticfiles")
+STATIC_ROOT= os.path.join(BASE_DIR,"staticfiles")
 
 STATIC_URL = '/static/'
 
@@ -129,3 +128,4 @@ MEDIA_URL="/images/"
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'statics/images')
 
+ 
